9058f4846804f5006542f5ba225ed97c551c52ab
Koorochka <artemij@koorochka.ru>
2019-07-15 13:02:09 +0300

    Pacagge to components

--- matrix/templates/.default/header.php
+++ matrix/templates/.default/header.php
@@ -29,63 +29,7 @@ $APPLICATION->ShowCSS();
 $APPLICATION->ShowHeadStrings();
 echo "</head>";
 echo "<body>";
+
+$APPLICATION->IncludeComponent("matrix:admin.header", null, null);
+$APPLICATION->IncludeComponent("matrix:admin.page", null, null);
 ?>
-
-<header id="header">
-
-    <nav class="navbar navbar-expand-xl navbar-dark bg-dark fixed-top shadow">
-        <a class="navbar-brand" href="#">penetron</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample06" aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarsExample06">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown06" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown06">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-            </ul>
-
-        </div>
-    </nav>
-
-</header>
-
-<div class="container-fluid">
-
-    <div class="row">
-        <div id="sidebur-activity"
-             class="col-md-2 d-none d-md-block bg-warning">
-            <?for($i=0; $i< 3; $i++):?>
-                <?d("sidebur")?>
-            <?endfor;?>
-
-
-
-        </div>
-        <main role="main"
-              class="col-md-9 ml-sm-auto col-lg-10 px-4"
-              id="main-activity">
-
-            <?for($i=0; $i< 6; $i++):?>
-                <?d("Main")?>
-            <?endfor;?>
-
-
-        </main>
-    </div>
-</div>
