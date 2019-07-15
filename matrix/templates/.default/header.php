0554547d047488e1cd029f6a2791f1b003751b7c
Koorochka <artemij@koorochka.ru>
2019-07-15 12:29:57 +0300

    Basic admin page in header

--- matrix/templates/.default/header.php
+++ matrix/templates/.default/header.php
@@ -32,15 +32,38 @@ echo "<body>";
 ?>
 
 <header id="header">
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
 
+        <ul class="navbar-nav px-3">
+            <li class="nav-item text-nowrap">
+                <a class="nav-link" href="#">Sign out</a>
+            </li>
+        </ul>
+    </nav>
 </header>
 
-<div id="main-activity">
+<div class="container-fluid">
 
-    <div class="container">
-        <div class="row">
-            <div class="col">COl</div>
+    <div class="row">
+        <div id="sidebur-activity"
+             class="col-md-2 d-none d-md-block bg-warning">
+            <?for($i=0; $i< 3; $i++):?>
+                <?d("sidebur")?>
+            <?endfor;?>
+
+
+
         </div>
-    </div>
+        <main role="main"
+              class="col-md-9 ml-sm-auto col-lg-10 px-4"
+              id="main-activity">
 
+            <?for($i=0; $i< 6; $i++):?>
+                <?d("Main")?>
+            <?endfor;?>
+
+
+        </main>
+    </div>
 </div>
