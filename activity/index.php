ae00639cd5b962dd3caabbe431264baf2bdc8a17
Koorochka <artemij@koorochka.ru>
2019-07-15 11:50:34 +0300

    Basic matrix page

---
+++ activity/index.php
@@ -0,0 +1,15 @@
+<?
+/**
+ * @var CMain $APPLICATION
+ * @var CUser $USER
+ */
+require_once($_SERVER["DOCUMENT_ROOT"]."/matrix/header.php");
+$APPLICATION->SetTitle("The main ativity");
+
+
+global $currentUrl;
+d($currentUrl);
+
+?>
+
+<?require_once($_SERVER["DOCUMENT_ROOT"]."/matrix/footer.php");?>
